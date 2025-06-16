c726353da8dc01c80cfad4e82dd4c8f4a818aeff
GopiWebDev <gopisv.webdev@gmail.com>
2025-06-16 23:44:24 +0530

    feat: refactor ChatList to use ChatCard component and add ripple effect animation

---
+++ src/components/Chat/ChatCard.tsx
@@ -0,0 +1,105 @@
+import type { Chat } from "@/types/Chat";
+import { Card } from "../ui/card";
+import { useRef } from "react";
+import { getAvatarColor } from "@/lib/avatarColor";
+
+const ChatCard = ({
+  chat,
+  selectedChat,
+  onSelect,
+}: {
+  chat: Chat;
+  selectedChat: Chat | null;
+  onSelect: (chat: Chat) => void;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleRipple = (
+    e: React.MouseEvent<HTMLDivElement>,
+    ref: React.RefObject<HTMLDivElement> | null
+  ) => {
+    const target = ref?.current;
+    if (!target) return;
+
+    const ripple = document.createElement("span");
+
+    const diameter = Math.max(target.clientWidth, target.clientHeight);
+    const radius = diameter / 2;
+
+    ripple.style.width = ripple.style.height = `${diameter}px`;
+    ripple.style.left = `${
+      e.clientX - target.getBoundingClientRect().left - radius
+    }px`;
+    ripple.style.top = `${
+      e.clientY - target.getBoundingClientRect().top - radius
+    }px`;
+    ripple.className =
+      "absolute bg-black/30 rounded-full animate-ripple pointer-events-none";
+
+    const existing = target.querySelector(".ripple-temp");
+    if (existing) existing.remove();
+
+    ripple.classList.add("ripple-temp");
+    target.appendChild(ripple);
+
+    setTimeout(() => ripple.remove(), 600);
+  };
+
+  return (
+    <Card
+      ref={ref}
+      key={chat.id}
+      className={`relative overflow-hidden px-4 py-2 cursor-pointer border-none h-[84px] bg-white dark:bg-[#212121] ${
+        selectedChat?.id === chat.id
+          ? "bg-[#3390ec] dark:bg-[#766ac8]"
+          : "hover:bg-[rgba(244,244,245)] dark:hover:bg-[rgba(44,44,44)]"
+      }`}
+      onClick={(e) => {
+        handleRipple(e, ref);
+        onSelect(chat);
+      }}
+    >
+      <div className="flex items-center gap-4 h-full">
+        {/* Avatar / Logo */}
+        <div
+          className={`w-[56px] h-[56px] rounded-full shrink-0 flex items-center justify-center text-lg font-medium text-white ${getAvatarColor(
+            chat.name
+          )}`}
+        >
+          {chat.name[0]}
+        </div>
+
+        {/* Chat Info */}
+        <div className="flex flex-col flex-1 min-w-0">
+          <div className="text-[16px] font-semibold text-foreground truncate flex justify-between items-center">
+            <p
+              className={`truncate ${
+                selectedChat?.id == chat.id ? "text-white" : ""
+              }`}
+            >
+              {chat.name}
+            </p>
+            <p
+              className={`text-[12px] font-light ${
+                selectedChat?.id == chat.id ? "text-white" : ""
+              }`}
+            >
+              {chat.time}
+            </p>
+          </div>
+          {chat.lastMessage && (
+            <div
+              className={`text-sm text-muted-foreground truncate ${
+                selectedChat?.id == chat.id ? "text-white" : ""
+              }`}
+            >
+              {chat.lastMessage}
+            </div>
+          )}
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ChatCard;
--- src/components/Chat/ChatList.tsx
+++ src/components/Chat/ChatList.tsx
@@ -1,7 +1,6 @@
-import { Card } from "@/components/ui/card";
 import type { Chat } from "@/types/Chat";
 import { DropDownMenu } from "./ChatListHeader/DropDownMenu";
-import { getAvatarColor } from "@/lib/avatarColor";
+import ChatCard from "./ChatCard";
 
 interface ChatListProps {
   chats: Chat[];
@@ -16,60 +15,17 @@ export default function ChatList({
 }: ChatListProps) {
   return (
     <div className="h-full flex flex-col gap-2 bg-white dark:bg-[#212121] z-10">
-      <div className="p-4 font-semibold text-lg border-b">
+      <div className="p-4 font-semibold text-lg">
         <DropDownMenu />
       </div>
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto px-2">
         {chats.map((chat) => (
-          <Card
+          <ChatCard
             key={chat.id}
-            className={`px-4 py-2 cursor-pointer border-none h-[84px] bg-white dark:bg-[#212121] ${
-              selectedChat?.id === chat.id
-                ? "bg-[#3390ec] dark:bg-[#766ac8]"
-                : "hover:bg-[rgba(244,244,245)] dark:hover:bg-[rgba(44,44,44)]"
-            }`}
-            onClick={() => onSelect(chat)}
-          >
-            <div className="flex items-center gap-4 h-full">
-              {/* Avatar / Logo */}
-              <div
-                className={`w-[56px] h-[56px] rounded-full shrink-0 flex items-center justify-center text-lg font-medium text-white ${getAvatarColor(
-                  chat.name
-                )}`}
-              >
-                {chat.name[0]}
-              </div>
-
-              {/* Chat Info */}
-              <div className="flex flex-col flex-1 min-w-0">
-                <div className="text-[16px] font-semibold text-foreground truncate flex justify-between items-center">
-                  <p
-                    className={`truncate ${
-                      selectedChat?.id == chat.id ? "text-white" : ""
-                    }`}
-                  >
-                    {chat.name}
-                  </p>
-                  <p
-                    className={`text-[12px] font-light ${
-                      selectedChat?.id == chat.id ? "text-white" : ""
-                    }`}
-                  >
-                    {chat.time}
-                  </p>
-                </div>
-                {chat.lastMessage && (
-                  <div
-                    className={`text-sm text-muted-foreground truncate ${
-                      selectedChat?.id == chat.id ? "text-white" : ""
-                    }`}
-                  >
-                    {chat.lastMessage}
-                  </div>
-                )}
-              </div>
-            </div>
-          </Card>
+            chat={chat}
+            selectedChat={selectedChat}
+            onSelect={onSelect}
+          />
         ))}
       </div>
     </div>
